61272d009d65b6438e6f2d519219915fbf686523
Brian Dillon <bdillon@live.ie>
2018-12-08 17:07:56 +0000

    Added new view controller charts class

---
+++ Billiard2017/Billiard2017/ChartsViewController.m
@@ -0,0 +1,25 @@
+//
+//  ChartsViewController.m
+//  Billiard2017
+//
+//  Created by Brian Dillon on 08/12/2018.
+//  Copyright © 2018 ROCUDO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+@import Charts;
+
+@interface ChartsViewController : UIViewController
+@property (weak, nonatomic) IBOutlet LineChartView *userDataLineChart;
+@end
+
+@implementation <#class#>
+
+
+
+@end
+
+
+
+
+
---
+++ Billiard2017/ChartsViewController.h
@@ -0,0 +1,9 @@
+#import <UIKit/UIKit.h>
+#import "ChartsViewController.h"
+
+@class ChartsViewController;
+
+@end
+@interface ChartsViewController : UIViewController
+
+@end
